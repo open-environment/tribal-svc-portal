d3ce06144fe3ac40c55f08297a5036350b356e67
Doug Timms <doug.timms@open-environment.org>
2018-07-02 13:20:26 -0400

    Add project files.

---
+++ TribalSvcPortal/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TribalSvcPortal.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
